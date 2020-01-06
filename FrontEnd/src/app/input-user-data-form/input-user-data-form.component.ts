ad5fca4c648d3deb4450c34db6eee89fb81503c2
lutfiqaraman <lutfiqaraman@gmail.com>
2020-01-06 22:30:17 +0200

    Creating Register Form

--- FrontEnd/src/app/input-user-data-form/input-user-data-form.component.ts
+++ FrontEnd/src/app/input-user-data-form/input-user-data-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-input-user-data-form',
@@ -6,10 +7,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input-user-data-form.component.css']
 })
 export class InputUserDataFormComponent implements OnInit {
+  registered = false;
+  submitted = false;
+  userForm: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
+
+  invalidName() {
+    return (this.submitted && this.userForm.controls.name.errors != null);
+  }
+
+  invalidEmail() {
+    return (this.submitted && this.userForm.controls.email.errors != null);
+  }
+
+  invalidZipcode() {
+    return (this.submitted && this.userForm.controls.zipcode.errors != null);
+  }
+
+  invalidPassword() {
+    return (this.submitted && this.userForm.controls.password.errors != null);
+  }
 
   ngOnInit() {
+    this.userForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      zipcode: ['', [Validators.required, Validators.pattern('^[0-9]{5}(?:-[0-9]{4})?$')]],
+      password: ['', [Validators.required, Validators.minLength(5), Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')]],
+    });
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.userForm.invalid) {
+      return;
+    } else {
+      this.registered = true;
+    }
   }
 
 }
