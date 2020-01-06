b3ce1940b1d33ad151445eda9596f7d941588007
lutfiqaraman <lutfiqaraman@gmail.com>
2020-01-06 22:55:46 +0200

    configure routing

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InputUserDataFormComponent } from './input-user-data-form/input-user-data-form.component';
+import { DisplayUserDataComponent } from './display-user-data/display-user-data.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: InputUserDataFormComponent
+  },
+  {
+    path: 'user/:uid',
+    component: DisplayUserDataComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
